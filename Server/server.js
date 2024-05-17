a3f1684fce152db44a437b467e37524621c7bf35
guybentley <guy@gdbentley.co.uk>
2024-05-17 16:22:57 +0100

    add in chalk and debug

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import { CardRepo } from "./cardRepo.js";
+import chalk from 'chalk';
+import morgan from 'morgan';
+import Debug from 'debug';
+
+const debug = Debug('CardOps');
 
 const url = 'mongodb://cardOpsAccess:IzmQvbcL247m4C87gG7AYGn@localhost:27017/';
 const dbName = 'CardOps';
 const cRepo = new CardRepo(url, dbName, 1);
-const app = express();
+const webServer = express();
 
-app.get('/', (req, res) =>{
+webServer.use(morgan('combined'));
+
+webServer.get('/', (req, res) =>{
     res.send('hello from CardOps')
 });
 
-app.listen(2883, () => console.log(`listening on port 2883`));
+webServer.listen(2883, () => { 
+    debug(`listening on port ${chalk.green('2883')}`)
+});
