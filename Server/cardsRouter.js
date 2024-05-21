a9e5edf8f487254fe9211635eb75c8841a3cc285
guybentley <guy@gdbentley.co.uk>
2024-05-21 19:43:15 +0100

    First set of working pages using EJS (eventually will replace with Angular). Test data loaded.

---
+++ Server/cardsRouter.js
@@ -0,0 +1,25 @@
+import express from 'express';
+// import { CardRepo } from "./cardRepo.js";
+import Debug from 'debug';
+import cards from '../TestData/cards.json' with { type: 'json'};
+
+const cardsRouter = express.Router();
+
+// try {
+//     const myProdCards = await cRepo.readCardsByProductId(`CardOps`);
+// } catch(err) {
+//     Debug.log(err.err);
+// }
+
+cardsRouter.route('/')
+    .get((req,res) => {
+        res.status(200).render('cards', {"cards": cards})
+    });
+
+cardsRouter.route('/:cardId').get((req, res) => {
+    const cardId = req.params.cardId;
+    const card = cards.find(c => c.cardId === cardId);
+    res.status(200).render('card', card )
+    });
+
+export default cardsRouter
--- Server/server.js
+++ Server/server.js
@@ -3,22 +3,26 @@ import { CardRepo } from "./cardRepo.js";
 import chalk from 'chalk';
 import morgan from 'morgan';
 import Debug from 'debug';
-import path from 'path';
+import cardsRouter from './cardsRouter.js';
 
 const debug = Debug('CardOps');
+const port = process.env.portNumber;
 
 const url = 'mongodb://cardOpsAccess:IzmQvbcL247m4C87gG7AYGn@localhost:27017/';
 const dbName = 'CardOps';
 const cRepo = new CardRepo(url, dbName, 1);
 const webServer = express();
 
+webServer.listen(port, () => { 
+    debug(`listening on port ${chalk.green(port)}`)
+});
+
 webServer.use(morgan('combined'));
-webServer.use(express.static(path.join('../Portal/')));
+webServer.set('views', './src/views');
+webServer.set('view engine', 'ejs');
 
 webServer.get('/', (req, res) =>{
-    res.send('hello from CardOps')
+    res.status(200).render('index', {title: 'CardOps', data: ['a', 'b', 'c'] } )
 });
 
-webServer.listen(2883, () => { 
-    debug(`listening on port ${chalk.green('2883')}`)
-});
+webServer.use('/cards', cardsRouter);
